640bc6b6ebb75908a1f73918d39a4d90d4f8a228
unknown <inessamanthapaulinel@gmail.com>
2023-08-28 17:51:42 +0800

    adding some changes to Hello.java

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ public class Hello
   {
     System.out.println("Hello, Software Engineering Course!");
     System.out.println("My name is INES, Samantha Pauline");
+    System.out.println("That is why developers are highly-paid!");
   }
 }
